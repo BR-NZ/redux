70b1e69037a14b51adae304d35d26731c48fcf41
Alexey Beschetnikoff <bronzspawn@gmail.com>
2023-06-09 02:55:25 +0700

    Простая реализация счетчика на Flux архитектуре с подключенной Redux библиотекой.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+
 import './index.css';
 
-const initialState = 0;
 
-const reducer = (state = 0, action) => {
+
+const initialState = { value: 0 };
+
+const reducer = (state = initialState, action) => {
     switch(action.type) {
         case "INC":
-            return state + 1;
+            return {
+                ...state,
+                value: state.value + 1
+            };
+        case "DEC":
+            return {
+                ...state,
+                value: state.value - 1
+            };
+        case "RND":
+            return {
+                ...state,
+                value: state.value * action.payload
+            };
         default:
             return state;
     }
 }
 
-let state = reducer(initialState, {type: "INC"});
-state = reducer(state, {type: "INC"});
-state = reducer(state, {type: "INC"});
-state = reducer(state, {type: "INC"});
+const update = () => {
+    document.getElementById("counter").textContent = store.getState().value;
+}
 
-console.log(state);
+// создаем объект action с помощью специальных функций actionCreators
+const rnd = () => ({ type: "RND", payload: Math.random() * 10 });
+
+
+
+
+const store = createStore(reducer);
+
+store.subscribe(update);
+
+
+
+
+document.getElementById("inc").addEventListener("click", () => {
+    // можно создавать объект action сразу при передаче
+    store.dispatch( {type: "INC"} );
+});
+
+document.getElementById("dec").addEventListener("click", () => {
+    store.dispatch( {type: "DEC"} );
+});
+
+document.getElementById("rnd").addEventListener("click", () => {
+    // а тут объект action получаем от actionCreators
+    store.dispatch( rnd() );
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <>
 
-    </React.StrictMode>
+    </>
 );
