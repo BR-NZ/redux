b93d23a0f06e0a35b8ff0589867d1508d193a820
Alexey Beschetnikoff <bronzspawn@gmail.com>
2023-06-12 00:58:26 +0700

    React добавлен в проект, счетик теперь компонент, создан Redux-store.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore, bindActionCreators } from 'redux';
-
-import * as actions from './actions';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reducer from './reducer';
+
+import App from './components/App';
 
 import './index.css';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 const store = createStore(reducer);
 
-const {dispatch, subscribe, getState} = store;
-
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-
-const update = () => {
-    document.getElementById("counter").textContent = getState().value;
-}
-subscribe(update);
-
-document.getElementById("inc").addEventListener("click", inc);
-
-document.getElementById("dec").addEventListener("click", dec);
-
-document.getElementById("rnd").addEventListener("click", () => {
-    const value = Math.random() * 10;
-    rnd(value);
-});
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
-
+        <Provider store={store}>
+            <App />
+        </Provider>
     </>
-);
+)
